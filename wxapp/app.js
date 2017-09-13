53bd9b740bb67680ccc85b82a34145a36ae8e40a
Pan, Xinyang <seanpan2008@gmail.com>
2017-09-13 12:35:07 +0800

    1

--- wxapp/app.js
+++ wxapp/app.js
@@ -1,15 +1,11 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+  onLaunch: function (res) {
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log('login: code=' + res.code);
       }
     })
     // 获取用户信息
@@ -20,7 +16,9 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
+
+              console.log('userInfo: ' + JSON.stringify(res.userInfo));
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
