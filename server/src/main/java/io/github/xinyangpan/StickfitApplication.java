f093db9a2d3bbf0d3a45fba5103de3c26fd9aa6c
Sean <Sean@Sean-Home-PC>
2017-09-24 02:24:00 +0800

    1

--- server/src/main/java/io/github/xinyangpan/StickfitApplication.java
+++ server/src/main/java/io/github/xinyangpan/StickfitApplication.java
@@ -3,8 +3,11 @@ package io.github.xinyangpan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
+
+import com.google.common.collect.Lists;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -14,6 +17,7 @@ public class StickfitApplication {
 //	@ConditionalOnMissingBean
 	public RestTemplate restTemplate() {
 		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setMessageConverters(Lists.newArrayList(new MappingJackson2HttpMessageConverter()));
 		return restTemplate;
 	}
 
