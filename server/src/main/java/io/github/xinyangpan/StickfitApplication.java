0f4bf90161e5d30fcb8f320923ddf617407c10d6
Sean <Sean@Sean-Home-PC>
2017-09-24 02:15:44 +0800

    1

--- server/src/main/java/io/github/xinyangpan/StickfitApplication.java
+++ server/src/main/java/io/github/xinyangpan/StickfitApplication.java
@@ -2,11 +2,20 @@ package io.github.xinyangpan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class StickfitApplication {
+
+	@Bean
+//	@ConditionalOnMissingBean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StickfitApplication.class, args);
--- server/src/main/java/io/github/xinyangpan/controller/CustomerController.java
+++ server/src/main/java/io/github/xinyangpan/controller/CustomerController.java
@@ -1,24 +1,35 @@
 package io.github.xinyangpan.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.xinyangpan.persistent.po.CustomerPo;
+import io.github.xinyangpan.service.CoreService;
 import io.github.xinyangpan.service.CustomerService;
+import io.github.xinyangpan.vo.Session;
 
 @RestController
 public class CustomerController {
+	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
+
+	@Autowired
+	private CoreService coreService;
 	@Autowired
 	private CustomerService customerService;
 
 	@GetMapping("/login")
 	public CustomerPo login(String code) {
-		
+		String appId = "wx7697e61e4407f57a";
+		String secret = "672fa10aea63e42a39a683c65267dca2";
+		Session session = coreService.jscode2session(appId, secret, code);
+		log.info("jscode2session: ", session);
 		return null;
 	}
 
-	@GetMapping("/login")
+	@GetMapping("/register")
 	public CustomerPo register(String openId, String username) {
 		return customerService.login(openId, username);
 	}
