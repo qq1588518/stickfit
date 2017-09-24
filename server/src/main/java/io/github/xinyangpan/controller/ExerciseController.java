56cf513b535df3148c030cba3b4c4c60504f784a
Sean <Sean@Sean-Home-PC>
2017-09-24 22:27:34 +0800

    1

--- server/src/main/java/io/github/xinyangpan/controller/ExerciseController.java
+++ server/src/main/java/io/github/xinyangpan/controller/ExerciseController.java
@@ -1,13 +1,12 @@
 package io.github.xinyangpan.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Lists;
 
 import io.github.xinyangpan.service.ExerciseService;
 import io.github.xinyangpan.vo.CurrentMonthHistory;
@@ -24,9 +23,9 @@ public class ExerciseController {
 	}
 
 	@PostMapping("/deleteExercisesByIds")
-	public void deleteExercisesByIds(@RequestBody List<Long> ids) {
+	public void deleteExercisesByIds(Long[] ids) {
 		System.out.println("deleteExercisesByIds" + ids);
-		exerciseService.deleteExercisesByIds(ids);
+		exerciseService.deleteExercisesByIds(Lists.newArrayList(ids));
 	}
 
 }
