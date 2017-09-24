a9e2ce56ae0c29c86d3295f843cbae6837960e1c
Sean <Sean@Sean-Home-PC>
2017-09-24 15:13:31 +0800

    1

--- server/src/main/java/io/github/xinyangpan/StickfitApplication.java
+++ server/src/main/java/io/github/xinyangpan/StickfitApplication.java
@@ -3,10 +3,14 @@ package io.github.xinyangpan;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,11 +19,21 @@ import org.springframework.web.client.RestTemplate;
 import com.google.common.collect.Lists;
 
 import io.github.xinyangpan.persistent.dao.ExerciseTypeDao;
+import io.github.xinyangpan.persistent.po.CustomerPo;
+import io.github.xinyangpan.persistent.po.ExercisePo;
 import io.github.xinyangpan.persistent.po.ExerciseTypePo;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class StickfitApplication {
+
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
+	@PostConstruct
+	public void init() {
+		repositoryRestConfiguration.exposeIdsFor(CustomerPo.class, ExercisePo.class, ExerciseTypePo.class);
+	}
 
 	@Bean
 	//	@ConditionalOnMissingBean
