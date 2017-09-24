671e1915092ea66a48001e673a622cf3159028ed
Sean <Sean@Sean-Home-PC>
2017-09-24 22:19:59 +0800

    1

--- server/src/main/java/io/github/xinyangpan/controller/ExerciseController.java
+++ server/src/main/java/io/github/xinyangpan/controller/ExerciseController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +23,8 @@ public class ExerciseController {
 		return exerciseService.currentMonthHistory(customerId);
 	}
 
-	@GetMapping("/deleteExercisesByIds")
-	public void deleteExercisesByIds(List<Long> ids) {
+	@PostMapping("/deleteExercisesByIds")
+	public void deleteExercisesByIds(@RequestBody List<Long> ids) {
 		System.out.println("deleteExercisesByIds" + ids);
 		exerciseService.deleteExercisesByIds(ids);
 	}
