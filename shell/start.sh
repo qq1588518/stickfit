348820c494f532c0e598f9c52b6ca0c8c0c6a8ab
Sean Pan <seanpan2008@gmail.com>
2017-10-04 22:30:18 +0800

    dev

--- shell/start.sh
+++ shell/start.sh
@@ -4,6 +4,10 @@ ENV=$1
 APP_NAME=stickfit
 APP_HOME=/app/${APP_NAME}
 
+if [ "${ENV}" -eq "dev" ]; then
+  APP_HOME=/appdev/${APP_NAME}
+fi
+
 mkdir -p ${APP_HOME}/log
 
 java -jar -Dspring.profiles.active=${ENV} -DAPP_NAME=${APP_NAME} ${APP_HOME}/${APP_NAME}.jar >> ${APP_HOME}/log/console.log 2>&1 &
