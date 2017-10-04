007bec5987b0f9449e298268136374f8e33c2064
Sean Pan <seanpan2008@gmail.com>
2017-10-04 22:46:57 +0800

    dev

--- shell/stop.sh
+++ shell/stop.sh
@@ -3,7 +3,7 @@
 ENV=$1
 APP_NAME=stickfit
 
-PID=`ps -ef | grep java | grep "APP_NAME=${APP_NAME} " | grep 'spring.profiles.active=${ENV}' | awk '{ print $2 }'`
+PID=`ps -ef | grep java | grep "APP_NAME=${APP_NAME} " | grep 'active=${ENV}' | awk '{ print $2 }'`
 
 if [ "${PID}" == "" ];then
 	echo "${APP_NAME} is not runing! skip stop. "
