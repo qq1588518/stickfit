1439ceb92174f9e8b6a7629b885a705c39d6971f
Pan, Xinyang <seanpan2008@gmail.com>
2017-10-10 11:01:26 +0800

    1

--- server/src/main/java/io/github/xinyangpan/controller/ExerciseController.java
+++ server/src/main/java/io/github/xinyangpan/controller/ExerciseController.java
@@ -41,7 +41,7 @@ public class ExerciseController {
 		// 
 		List<LocalDate> localDates = Lists.newArrayList();
 		while (true) {
-			if (current.getMonthValue() >= now.getMonthValue()) {
+			if (current.getMonthValue() > now.getMonthValue()) {
 				break;
 			}
 			localDates.add(current);
