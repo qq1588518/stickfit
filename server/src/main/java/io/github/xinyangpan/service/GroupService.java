0af29e7866de987a2eb61ac4cce996c94a7e3227
Pan, Xinyang <seanpan2008@gmail.com>
2017-10-11 14:53:20 +0800

    1

--- server/src/main/java/io/github/xinyangpan/service/GroupService.java
+++ server/src/main/java/io/github/xinyangpan/service/GroupService.java
@@ -3,6 +3,8 @@ package io.github.xinyangpan.service;
 import java.util.List;
 import java.util.Objects;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -14,6 +16,7 @@ import io.github.xinyangpan.persistent.po.CustomerPo;
 import io.github.xinyangpan.persistent.po.GroupPo;
 
 @Service
+@Transactional
 public class GroupService {
 	@Autowired
 	private ExerciseDao exerciseDao;
