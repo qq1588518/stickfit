cd6079abd1d22ce30c0eda8e3c47d6b08f9f8d26
Pan, Xinyang <seanpan2008@gmail.com>
2017-12-01 15:51:17 +0800

    1

--- doc/init.sql
+++ doc/init.sql
@@ -5,3 +5,7 @@ insert into exercise_type_po(`name`,`description`,`min`,`unit`,`order`) values
 ('骑车','骑车',12,'公里',4),
 ('其他','其他运动',45,'分钟',99);
 
+
+insert into month_standard(group_id, month, standard_enum, standard_param) values(1, 201709, 'Count', '15');
+insert into month_standard(group_id, month, standard_enum, standard_param) values(1, 201710, 'JogAmountOrMix', '180,100,15,9');
+insert into month_standard(group_id, month, standard_enum, standard_param) values(1, 201711, 'JogAmountOrMix', '180,110,15,9');
