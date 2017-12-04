36fe97d21eb8f52bc9ced5e0c9dd7ed0656765d0
Pan, Xinyang <seanpan2008@gmail.com>
2017-12-04 12:19:35 +0800

    1

--- doc/init.sql
+++ doc/init.sql
@@ -1,3 +1,5 @@
+delete from month_standard;
+ALTER TABLE month_standard AUTO_INCREMENT = 1;
 insert into exercise_type_po(`name`,`description`,`min`,`unit`,`order`) values 
 ('跑步','跑步',6,'公里',1),
 ('走步','走步',10000,'步',2),
@@ -10,7 +12,8 @@ ALTER TABLE group_po AUTO_INCREMENT = 1;
 insert into group_po(name, owner_id) values('未分组', 0);
 insert into group_po(name, owner_id) values('下一马', 3);
 
-drop table month_standard;
+delete from month_standard;
+ALTER TABLE month_standard AUTO_INCREMENT = 1;
 insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(1, 201709, 'Count', '15', null);
 insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(1, 201710, 'JogAmountOrMix', '180,100,15,9', null);
 insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(1, 201711, 'JogAmountOrMix', '180,110,15,9', 12);
--- doc/rel-2.1.sql
+++ doc/rel-2.1.sql
@@ -1,1 +1,14 @@
+delete from group_po;
+ALTER TABLE group_po AUTO_INCREMENT = 1;
+insert into group_po(name, owner_id) values('未分组', 0);
+insert into group_po(name, owner_id) values('下一马', 3);
+
+delete from month_standard;
+ALTER TABLE month_standard AUTO_INCREMENT = 1;
+insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(2, 201709, 'Count', '15', null);
+insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(2, 201710, 'JogAmountOrMix', '180,100,15,9', null);
+insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(2, 201711, 'JogAmountOrMix', '180,110,15,9', 12);
+insert into month_standard(group_id, month, standard_enum, standard_param, pioneer_id) values(2, 201712, 'JogAmountOrMix', '180,110,15,9', 21);
+
 update customer_po set group_id=2 where id in(1,3,4,5,7,8,9,10,11,12,13,14,15,16,19,20,21,31);
+update exercise_po set group_id=2 where customer_id in(1,3,4,5,7,8,9,10,11,12,13,14,15,16,19,20,21,31);
