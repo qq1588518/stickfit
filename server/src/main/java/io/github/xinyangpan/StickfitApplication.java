db0de6497ee8275fa0b0cfff161580ca46962400
Pan, Xinyang <seanpan2008@gmail.com>
2017-12-29 15:24:29 +0800

    add swagger

--- server/src/main/java/io/github/xinyangpan/StickfitApplication.java
+++ server/src/main/java/io/github/xinyangpan/StickfitApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -20,9 +21,20 @@ import io.github.xinyangpan.persistent.po.CustomerPo;
 import io.github.xinyangpan.persistent.po.ExercisePo;
 import io.github.xinyangpan.persistent.po.ExerciseTypePo;
 import io.github.xinyangpan.persistent.po.GroupPo;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 public class StickfitApplication {
 
 	@Autowired
@@ -33,6 +45,26 @@ public class StickfitApplication {
 		repositoryRestConfiguration.exposeIdsFor(CustomerPo.class, GroupPo.class, ExercisePo.class, ExerciseTypePo.class);
 	}
 
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.github.xinyangpan"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("下一马打卡App")
+                .description("下一马打卡App后台程序")
+                .termsOfServiceUrl("https://www.nextmarathon.cn/")
+                .contact(new Contact("Pan Xinyang", "", "seanpan2008@gmail.com"))
+                .version("2.1")
+                .build();
+    }
+    
 	@Bean
 	//	@ConditionalOnMissingBean
 	public RestTemplate restTemplate() {
--- server/src/main/java/io/github/xinyangpan/controller/CoreController.java
+++ server/src/main/java/io/github/xinyangpan/controller/CoreController.java
@@ -4,17 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/core")
 public class CoreController {
 
 	private volatile long cacheTimestamp = System.currentTimeMillis();
 
+    @ApiOperation(value="获取当前服务端版本号", notes="")
 	@GetMapping("/version")
 	public String version() {
 		return "2.0.1";
 	}
 
+    @ApiOperation(value="获取Cache刷新时间", notes="")
 	@GetMapping("/cacheTimestamp")
 	public long cacheTimestamp(boolean renew) {
 		if (renew) {
